4bd30bea18b34d7bdf5ac3de165950ea1dadf77b
Oswaldo Vargas <oswaldo.vargas@udg.mx>
2025-06-19 15:40:59 -0600

    Consultas N+1

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -15,6 +15,7 @@ class CategoryController extends Controller
     public function index()
     {
         $categories = Category::orderBy('id', 'desc')
+            ->with('family')
             ->paginate(10);
         return view('admin.categories.index', compact('categories'));
     }
